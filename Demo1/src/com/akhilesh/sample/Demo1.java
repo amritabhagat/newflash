2f6a5d70d6b95b5ab142090afd652a395e7e8567
Akhilesh <abhagat@finfort.ind.in>
2019-02-06 12:00:08 +0530

    new flash branch added in

--- Demo1/src/com/akhilesh/sample/Demo1.java
+++ Demo1/src/com/akhilesh/sample/Demo1.java
@@ -3,6 +3,7 @@ package com.akhilesh.sample;
 public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("Demo-1");
+		System.out.println("New flash child added in branch Demo-1");
 		
 	}
 
