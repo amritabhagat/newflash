2f0433bf775f26887207bc199975a9b2af1705e3
Akhilesh <abhagat@finfort.ind.in>
2019-02-06 12:11:44 +0530

    demo updated

--- Demo1/src/com/akhilesh/sample/Demo1.java
+++ Demo1/src/com/akhilesh/sample/Demo1.java
@@ -4,6 +4,9 @@ public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("Demo-1");
 		System.out.println("New flash child added in branch Demo-1");
+		System.out.println("new branch");
+		System.out.println("Final branch");
+		
 		
 	}
 
